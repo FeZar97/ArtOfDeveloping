ae4b299ee1f72a3dd0de68b8d358192b73232fc1
FeZar97 <bhd2010@yandex.ru>
2020-11-24 00:49:37 +0300

    complete final and week5; complete white belt;

--- WhiteBelt/WhiteBeltFinal/main.cpp
+++ WhiteBelt/WhiteBeltFinal/main.cpp
@@ -52,17 +52,41 @@ public:
 		is >> inputString;
 		ss << inputString;
 
-		ss >> _year >> _delim1 >> _month >> _delim2 >> _day;
-		if (_delim1 != '-' || _delim2 != '-' || (ss.peek() != EOF && ss.peek() != '\n')) {
-			// error_message = "Wrong date format: " + to_string(_year) + _delim1 + to_string(_month) + _delim2 + to_string(_day);
+		ss >> _year;
+		if (!ss.good() || ss.eof()) {
 			error_message = "Wrong date format: " + inputString;
 		}
-		else {
+
+		ss >> _delim1;
+		if (!ss.good() || _delim1 != '-' || ss.eof()) {
+			error_message = "Wrong date format: " + inputString;
+		}
+
+		ss >> _month;
+		if (!ss.good() || ss.eof()) {
+			error_message = "Wrong date format: " + inputString;
+		}
+
+		ss >> _delim2;
+		if (!ss.good() || _delim2 != '-' || ss.peek() == EOF) {
+			error_message = "Wrong date format: " + inputString;
+		}
+
+		ss >> _day;
+		// if (!ss.eof()) { // || (!ss.good() && !ss.eof())) {
+		// 	error_message = "Wrong date format: " + inputString;
+		// }
+
+		if (ss.peek() != EOF) {
+			error_message = "Wrong date format: " + inputString;
+		}
+
+		if (error_message.empty()) {
 			if (_month > 0 && _month < 13) {
-				if (_day > 0 && _day < 32) {
-					date.year  = _year;
+				if (_day > 0 && _day < 32 && ss.eof()) {
+					date.year = _year;
 					date.month = _month;
-					date.day   = _day;
+					date.day = _day;
 				}
 				else {
 					error_message = "Day value is invalid: " + to_string(_day);
@@ -72,6 +96,7 @@ public:
 				error_message = "Month value is invalid: " + to_string(_month);
 			}
 		}
+
 		if (!error_message.empty()) {
 			throw runtime_error(error_message);
 		}
@@ -130,7 +155,113 @@ public:
 	}
 };
 
+vector<pair<string, string>> Tests{
+	{"Add 1-1- event0", "Wrong date format: 1-1-\n"},
+	{"Add 1-1-t event1", "Day value is invalid: 0\n"},
+	{"Add 1-1-1) event2", "Wrong date format: 1-1-1)\n"},
+	{"Add 1-t-1 event3", "Wrong date format: 1-t-1\n"},
+	{"Add t-1-1 event4", "Wrong date format: t-1-1\n"},
+	{"Add 1--1-+1 event5", "Month value is invalid: -1\n"},
+	{"Add -1--1-+1 event6", "Month value is invalid: -1\n"},
+	{"Add -1--1--1 event7", "Month value is invalid: -1\n"},
+	{"Add -1-+1--1 event8", "Day value is invalid: -1\n"},
+	{"Add -1-+13--1 event9", "Month value is invalid: 13\n"},
+	{"Add -1-+12-32 event10", "Day value is invalid: 32\n"},
+	{"Add -1-+12-+32 event11", "Day value is invalid: 32\n"},
+	{"Add 1-1-1- event12", "Wrong date format: 1-1-1-\n"},
+	{"Add 1-1-+t event14", "Day value is invalid: 0\n"},
+	{"Add 1-1--t event14", "Day value is invalid: 0\n"},
+
+	{"Add 1-1-1 1event", ""},
+	{"Add 1-1-+1 2event", ""},
+	{"Add -1-+1-+1 3event", ""},
+	{"Add -1-+1-+1 4event", ""},
+};
+
+int CoreFunction(string input, string &answer) {
+	Database db;
+	Date date;
+	string line = input, command, event;
+	stringstream ss, answerstream;
+
+	try {
+		if (line.empty() || line == "\n") {
+			answer = "";
+			return 0;
+		}
+		ss.clear();
+		ss << line;
+		ss >> command;
+
+		if (command == "Add") {
+			ss >> date >> event;
+			db.AddEvent(date, event);
+			answer = "";
+			return 0;
+		}
+		else if (command == "Del") {
+			ss >> date;
+
+			if (ss.peek() == '\n' || ss.peek() == EOF) {
+				int delEvNb = db.DeleteDate(date);
+				answerstream << "Deleted " << delEvNb << " events" << endl;
+				answer = answerstream.str();
+				return 0;
+			}
+			else {
+				ss >> event;
+				if (db.DeleteEvent(date, event)) {
+					answerstream << "Deleted successfully" << endl;
+					answer = answerstream.str();
+					return 0;
+				}
+				else {
+					answerstream << "Event not found" << endl;
+					answer = answerstream.str();
+					return 0;
+				}
+			}
+		}
+		else if (command == "Find") {
+			ss >> date;
+			auto dateEvent = db.Find(date);
+			for (auto event : dateEvent) {
+				answerstream << event << endl;
+			}
+			answer = answerstream.str();
+			return 0;
+		}
+		else if (command == "Print") {
+			db.Print();
+			answer = "";
+			return 0;
+		}
+		else {
+			answerstream << "Unknown command: " + command << endl;
+			answer = answerstream.str();
+			return -1;
+		}
+	}
+	catch (exception &ex) {
+		answerstream << ex.what() << endl;
+		answer = answerstream.str();
+		return -1;
+	}
+}
+
+
 int main() {
+
+	/*
+	for (size_t i = 0; i < Tests.size(); i++) {
+		string answer;
+		int result = CoreFunction(Tests[i].first, answer);
+		if (answer != Tests[i].second) {
+			cout << "Error in test #" << i << "\n\t" << answer << "\t" << Tests[i].second << endl;
+		}
+	}
+	*/
+
 	Database db;
 	Date date;
 	string line, command, event;
@@ -186,8 +317,6 @@ int main() {
 			break;
 		}
 	}
-
-	system("pause");
 
 	return 0;
 }
