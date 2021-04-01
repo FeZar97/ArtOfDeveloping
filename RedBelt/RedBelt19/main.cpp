8c55d180da9845a74cb9aba6b7586871dbe8bc76
FeZar97 <bhd2010@yandex.ru>
2021-04-02 01:01:52 +0300

    complete rb week 1

--- RedBelt/RedBelt19/main.cpp
+++ RedBelt/RedBelt19/main.cpp
@@ -3,17 +3,76 @@
 #include <vector>
 #include <string>
 
+#include <list>
 #include "../test_runner.h"
 
 using namespace std;
 
 template <typename Iterator>
+class IteratorRange {
+    Iterator first, last;
+
+public:
+    IteratorRange(Iterator f, Iterator l) : first(f), last(l) {
+    }
+
+    Iterator begin() const {
+        return first;
+    }
+
+    Iterator end() const {
+        return last;
+    }
+
+    size_t size() const {
+        return last - first;
+    }
+};
+
+template <typename Iterator>
+auto MakeRange(Iterator begin, Iterator end) {
+    return IteratorRange<Iterator>(begin, end);
+}
+
+template <typename Iterator>
 class Paginator {
+
+    vector<IteratorRange<Iterator>> pages;
+
+public:
+    Paginator(Iterator begin, Iterator end, size_t page_size) {
+        pages.clear();
+
+        for (Iterator start = begin; start < end;) {
+            size_t rangeLength = ( (end - start) < page_size ? (end - start) : page_size);
+            IteratorRange<Iterator> nextRange = MakeRange<Iterator>(start, start + rangeLength);
+            pages.push_back(nextRange);
+
+            if (rangeLength == page_size) {
+                start += page_size;
+            }
+            else {
+                break;
+            }
+        }
+    }
+
+    auto begin() const {
+        return pages.begin();
+    }
+
+    auto end() const {
+        return pages.end();
+    }
+
+    size_t size() const {
+        return pages.size();
+    }
 };
 
 template <typename C>
-? ? ? Paginate(C & c, size_t page_size) {
-
+auto Paginate(C &c, size_t page_size) {
+    return Paginator(c.begin(), c.end(), page_size);
 }
 
 void TestPageCounts() {
@@ -107,7 +166,27 @@ void TestPagePagination() {
     ASSERT_EQUAL(lines, expected);
 }
 
+void TestFromCoursera1() {
+    vector<int> v1;
+    ASSERT_EQUAL(Paginate(v1, 1).size(), 0);
+}
+
 int main() {
+
+    vector<int> v = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+    auto result = Paginate(v, 5);
+
+    int pageIdx = 0;
+    for (auto page : result) {
+        cout << "Page " << pageIdx << ": ";
+        for (auto elem : page) {
+            cout  << elem << ", ";
+        }
+        pageIdx++;
+        cout << endl;
+    }
+
     TestRunner tr;
     RUN_TEST(tr, TestPageCounts);
     RUN_TEST(tr, TestLooping);
@@ -115,4 +194,5 @@ int main() {
     RUN_TEST(tr, TestPageSizes);
     RUN_TEST(tr, TestConstContainer);
     RUN_TEST(tr, TestPagePagination);
+    RUN_TEST(tr, TestFromCoursera1);
 }
--- RedBelt/test_runner.h
+++ RedBelt/test_runner.h
@@ -97,11 +97,11 @@ private:
     int fail_count = 0;
 };
 
-#define ASSERT_EQUAL(x, y) {            \
-  ostringstream os;                     \
-  os << #x << " != " << #y << ", "      \
-    << __FILE__ << ":" << __LINE__;     \
-  AssertEqual(x, y, os.str());          \
+#define ASSERT_EQUAL(x, y) {                    \
+  ostringstream myOsStream;                     \
+  myOsStream << #x << " != " << #y << ", "      \
+    << __FILE__ << ":" << __LINE__;             \
+  AssertEqual(x, y, myOsStream.str());          \
 }
 
 #define ASSERT(x) {                     \
