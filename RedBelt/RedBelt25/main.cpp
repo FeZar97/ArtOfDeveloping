4d9322cb795237e89327605aa5464507cf52c271
FeZar97 <bhd2010@yandex.ru>
2021-04-06 02:38:05 +0300

    rb25 wip

--- RedBelt/RedBelt25/main.cpp
+++ RedBelt/RedBelt25/main.cpp
@@ -2,8 +2,11 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <deque>
 #include <set>
 #include <utility>
+
+// #include "../test_runner.h"
 
 using namespace std;
 
@@ -15,7 +18,7 @@ class RoomBooking {
 
 public:
     RoomBooking() {}
-    RoomBooking(const string& hotelName_, int64_t reserveTime_, int64_t clientId_, unsigned int roomCount_) {
+    RoomBooking(int64_t reserveTime_, const string& hotelName_, int64_t clientId_, unsigned int roomCount_) {
         hotelName = hotelName_;
         reserveTime = reserveTime_;
         clientId = clientId_;
@@ -34,6 +37,14 @@ public:
         return reserveTime > rm.reserveTime;
     }
 
+    bool operator<=(const RoomBooking& rm) const {
+        return (*this < rm) || (*this == rm);
+    }
+
+    string getHotelName() const {
+        return hotelName;
+    }
+
     int64_t getResrveTime() const {
         return reserveTime;
     }
@@ -47,25 +58,56 @@ public:
     }
 };
 
-class BookingManager {
-    multiset<RoomBooking> reservs;
-    map<string, multiset<RoomBooking>> hotelReserves;
+class HotelManager {
+    deque<RoomBooking> reservs;
+    map<string, uint64_t> hotelRoomsReserves;
+    // _clients[hotel_name][client_id] += room_count;
+    // _rooms[hotel_name] += room_count;
+
+    auto dequeBinarySearchPos(const deque<RoomBooking> &reservs, const RoomBooking& elem) {
+        auto beginIt = reservs.begin(),
+            endIt = reservs.end(),
+            middleIt = reservs.begin();
+        uint64_t curLength = distance(reservs.begin(), reservs.end());
+
+        while (curLength) {
+            curLength = distance(beginIt, endIt) / 2;
+            middleIt = beginIt + curLength;
+
+            if (*middleIt > elem) {
+                endIt = middleIt;
+            }
+            else if (*middleIt <= elem) {
+                beginIt = middleIt;
+            }
+        }
+        return endIt;
+    }
+
+    void removeOldReserves(deque<RoomBooking> &reserves, const RoomBooking& elem) {
+        RoomBooking removeThreshold(elem.getResrveTime() - 86400, "", 0, 0);
+        auto thresholdIt = dequeBinarySearchPos(reserves, removeThreshold);
+        reserves.erase(reserves.begin(), thresholdIt);
+    }
 
 public:
-    BookingManager() {}
+    HotelManager() {}
 
-    void addReserve(const string& hotelName_, int64_t reserveTime_, int64_t clientId_, int roomCount_) {
-        RoomBooking rb(hotelName_, reserveTime_, clientId_, roomCount_);
-        reservs.insert(rb);
-        hotelReserves[hotelName_].insert(rb);
+    void addReserve(RoomBooking rb) {
+        addReserve(rb.getResrveTime(), rb.getHotelName(), rb.getClientId(), rb.getRoomCount());
+    }
 
-        int64_t timeThreshold = (*reservs.rbegin()).getResrveTime() - 86400;
-        auto firstCorrectIt = reservs.upper_bound(RoomBooking("", timeThreshold, 0, 0));
+    void addReserve(int64_t reserveTime_, const string& hotelName_, int64_t clientId_, int roomCount_) {
+        RoomBooking rb(reserveTime_, hotelName_, clientId_, roomCount_);
 
-        reservs.erase(reservs.begin(), firstCorrectIt);
-        for (auto& hotelPair : hotelReserves) {
-            auto firstCorrectHotelIt = hotelPair.second.upper_bound(RoomBooking("", timeThreshold, 0, 0));
-            hotelPair.second.erase(hotelPair.second.begin(), firstCorrectHotelIt);
+        // insert into reservs
+        if (!reservs.empty()) {
+            auto bestInsertIt = dequeBinarySearchPos(reservs, rb);
+            reservs.insert(bestInsertIt, rb);
+            removeOldReserves(reservs, rb);
+        }
+        else {
+            reservs.push_back(rb);
         }
     }
 
@@ -74,16 +116,10 @@ public:
             return 0;
         }
 
-        const multiset<RoomBooking> &curHotelReserves = hotelReserves.at(hotelName);
+        const deque<RoomBooking> &curHotelReserves = hotelReserves.at(hotelName);
         set<int64_t> uniqClients;
 
-        int64_t timeThreshold = (*reservs.rbegin()).getResrveTime() - 86400;
-
-        for (auto curIt = curHotelReserves.rbegin(); curIt != curHotelReserves.rend(); curIt++) {
-
-            if ((*curIt).getResrveTime() <= timeThreshold) {
-                break;
-            }
+        for (auto curIt = curHotelReserves.begin(); curIt != curHotelReserves.end(); curIt++) {
             uniqClients.insert((*curIt).getClientId());
         }
         return uniqClients.size();
@@ -94,28 +130,156 @@ public:
             return 0;
         }
 
-        const multiset<RoomBooking> &curHotelReserves = hotelReserves.at(hotelName);
+        const deque<RoomBooking> &curHotelReserves = hotelReserves.at(hotelName);
         int roomsCnt = 0;
 
-        int64_t timeThreshold = (*reservs.rbegin()).getResrveTime() - 86400;
-
-        for (auto curIt = curHotelReserves.rbegin(); curIt != curHotelReserves.rend(); curIt++) {
-
-            if ((*curIt).getResrveTime() <= timeThreshold) {
-                break;
-            }
+        for (auto curIt = curHotelReserves.begin(); curIt != curHotelReserves.end(); curIt++) {
             roomsCnt += (*curIt).getRoomCount();
         }
         return roomsCnt;
     }
 };
 
+/*
+void Test1() {
+    HotelManager hm;
+    ASSERT_EQUAL(hm.getClients("M"), 0);
+    ASSERT_EQUAL(hm.getRooms("M"), 0);
+    hm.addReserve({ 10,"F",1,2 });
+    hm.addReserve({ 10,"M",1,1 });
+    hm.addReserve({ 86409,"F",2,1 });
+    ASSERT_EQUAL(hm.getClients("F"), 2);
+    ASSERT_EQUAL(hm.getRooms("F"), 3);
+    ASSERT_EQUAL(hm.getClients("M"), 1);
+    hm.addReserve({ 86410,"M",2,10 });
+    ASSERT_EQUAL(hm.getRooms("F"), 1);
+    ASSERT_EQUAL(hm.getRooms("M"), 10);
+}
+void Test2() {
+    HotelManager hm;
+
+    ASSERT_EQUAL(hm.getRooms("a"), 0);
+    ASSERT_EQUAL(hm.getClients("a"), 0);
+    hm.addReserve({ -100000, "a", 100000, 1000 });
+    hm.addReserve({ -100000, "a", 100003, 1000 });
+    hm.addReserve({ -100000, "b", 100002, 1000 });
+    ASSERT_EQUAL(hm.getRooms("a"), 2000);
+    ASSERT_EQUAL(hm.getClients("a"), 2);
+    hm.addReserve({ -10000, "a", 100002, 1000 });
+    ASSERT_EQUAL(hm.getRooms("a"), 1000);
+    ASSERT_EQUAL(hm.getClients("a"), 1);
+    ASSERT_EQUAL(hm.getRooms("b"), 0);
+    ASSERT_EQUAL(hm.getClients("b"), 0);
+}
+void Test3() {
+    HotelManager manager;
+
+    // Test empty bookings
+    ASSERT_EQUAL(manager.getClients("mariott"), 0);
+    ASSERT_EQUAL(manager.getRooms("mariott"), 0);
+
+    manager.addReserve({ 0, "mariott", 1, 10 });
+    manager.addReserve({ 0, "mariott", 2, 1 });
+    manager.addReserve({ 0, "mariott", 3, 1 });
+    manager.addReserve({ 0, "mariott", 4, 1 });
+    manager.addReserve({ 0, "hilton", 1, 1 });
+    manager.addReserve({ 1, "hilton", 2, 1 });
+
+    // Test correctness
+    ASSERT_EQUAL(manager.getClients("mariott"), 4);
+    ASSERT_EQUAL(manager.getRooms("mariott"), 13);
+    ASSERT_EQUAL(manager.getClients("hilton"), 2);
+    ASSERT_EQUAL(manager.getRooms("hilton"), 2);
+
+    // Test event past 1 day resets statics
+    manager.addReserve({ 86400, "mariott", 1, 1 });
+    ASSERT_EQUAL(manager.getClients("mariott"), 1);
+    ASSERT_EQUAL(manager.getRooms("mariott"), 1);
+    ASSERT_EQUAL(manager.getClients("hilton"), 1);
+    ASSERT_EQUAL(manager.getRooms("hilton"), 1);
+
+    // Test no clients and room for the last day
+    manager.addReserve({ 86401, "mariott", 5, 1 });
+    ASSERT_EQUAL(manager.getClients("mariott"), 2);
+    ASSERT_EQUAL(manager.getRooms("mariott"), 2);
+    ASSERT_EQUAL(manager.getClients("hilton"), 0);
+    ASSERT_EQUAL(manager.getRooms("hilton"), 0);
+}
+void Test4() {
+    HotelManager b;
+
+    ASSERT_EQUAL(b.getRooms("a"), 0);
+    ASSERT_EQUAL(b.getClients("a"), 0);
+
+    ASSERT_EQUAL(b.getRooms("b"), 0);
+    ASSERT_EQUAL(b.getClients("c"), 0);
+
+    b.addReserve({ -100000,"a",100000,1000 });
+    b.addReserve({ -100000,"a",100003,1000 });
+    b.addReserve({ -100000,"b",100002,1000 });
+    ASSERT_EQUAL(b.getRooms("a"), 2000);
+    ASSERT_EQUAL(b.getClients("a"), 2);
+
+    b.addReserve({ -10000,"a",100002,1000 });
+    ASSERT_EQUAL(b.getRooms("a"), 1000);
+    ASSERT_EQUAL(b.getClients("a"), 1);
+    ASSERT_EQUAL(b.getRooms("b"), 0);
+    ASSERT_EQUAL(b.getClients("b"), 0);
+}
+void Test5() {
+    HotelManager b;
+
+    b.addReserve({ 0, "a", 0, 1 });
+    b.addReserve({ 1, "a", 1, 2 });
+    b.addReserve({ 86400, "a", 1, 3 });
+    ASSERT_EQUAL(b.getClients("a"), 1);
+    ASSERT_EQUAL(b.getRooms("a"), 5);
+}
+void Test6() {
+    HotelManager b;
+
+    b.addReserve({ -86400, "a", 2, 2 });
+    b.addReserve({ 0, "a", 1, 5 });
+    ASSERT_EQUAL(b.getClients("a"), 1);
+    ASSERT_EQUAL(b.getRooms("a"), 5);
+}
+void Test7() {
+    HotelManager b;
+
+    b.addReserve({ 1, "a", 11, 4 });
+    b.addReserve({ 50000, "a", 11, 5 });
+    b.addReserve({ 90000, "a", 22, 2 });
+    ASSERT_EQUAL(b.getClients("a"), 2);
+    ASSERT_EQUAL(b.getRooms("a"), 7);
+}
+void Test8() {
+    HotelManager b;
+
+    b.addReserve({ 2147483647, "a", 1, 3 });
+    b.addReserve({ 2147483648, "a", 2, 11 });
+    b.addReserve({ 4147483648, "a", 3, 13 });
+    ASSERT_EQUAL(b.getClients("a"), 1);
+    ASSERT_EQUAL(b.getRooms("a"), 13);
+}
+
+void AllTests() {
+    TestRunner tr;
+    RUN_TEST(tr, Test1);
+    RUN_TEST(tr, Test2);
+    RUN_TEST(tr, Test3);
+    RUN_TEST(tr, Test4);
+    RUN_TEST(tr, Test5);
+    RUN_TEST(tr, Test6);
+    RUN_TEST(tr, Test7);
+    RUN_TEST(tr, Test8);
+}
+*/
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    BookingManager manager;
+    HotelManager manager;
 
     int query_count;
     cin >> query_count;
@@ -129,7 +293,7 @@ int main() {
 
         if (queryType == "BOOK") {
             cin >> reserveTime >> hotelName >> clientId >> roomCount;
-            manager.addReserve(hotelName, reserveTime, clientId, roomCount);
+            manager.addReserve(reserveTime, hotelName, clientId, roomCount);
         }
         else if (queryType == "CLIENTS") {
             cin >> hotelName;
